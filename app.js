cdc69f8aee8f020fdc67755f5d8e14212fe61075
swadhi03 <swathiksanthosh03@gmail.com>
2024-06-13 22:15:25 +0530

    completed

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 const { usermodel } = require("./models/user")
-
 
 const app = express()
 app.use(cors())
@@ -10,8 +11,17 @@ app.use(express.json())
 
 mongoose.connect("mongodb+srv://swathi:swathi2609@cluster0.em0miqo.mongodb.net/busdb?retryWrites=true&w=majority&appName=Cluster0")
 
-app.post("/signup", (req, res) => {
+
+const generateHashedPassword = async (password)=>{
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(password,salt)
+}
+
+
+app.post("/signup", async (req, res) => {
     let input = req.body
+    let hashedPassword = await generateHashedPassword(input.password)
+    console.log(hashedPassword)
     let user = new usermodel(input)
     user.save()
     console.log(user)
