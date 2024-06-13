d77fccb377f9e6b68a8a9fce9a8b17fd35729bc3
swadhi03 <swathiksanthosh03@gmail.com>
2024-06-13 22:41:36 +0530

    completed

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 const { usermodel } = require("./models/user")
 
 
@@ -10,22 +12,54 @@ app.use(express.json())
 
 mongoose.connect("mongodb+srv://swathi:swathi2609@cluster0.em0miqo.mongodb.net/busdb?retryWrites=true&w=majority&appName=Cluster0")
 
-app.post("/signup", (req, res) => {
+const generateHashedPassword = async (password)=>{
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(password,salt)
+}
+
+app.post("/signup",async (req,res)=>{
     let input = req.body
+    let hashedPassword = await generateHashedPassword(input.password)
+    console.log(hashedPassword)
+    input.password=hashedPassword
     let user = new usermodel(input)
     user.save()
     console.log(user)
-    res.json({ "status": "success" })
+
+    res.json({"status":"success"})
 })
 
-app.post("/signin", (req, res) => {
+
+app.post("/signin",(req,res)=>{
     let input = req.body
-    let user = new usermodel(input)
-    user.save()
-    console.log(user)
-    res.json({ "status": "success" })
+    usermodel.find({"email":req.body.email}).then(
+        (response)=>{
+            if(response.length>0){
+                let dbPassword = response[0].password
+                console.log(dbPassword)
+                bcrypt.compare(input.password,dbPassword,(error,isMatch)=>{
+                    if(isMatch){
+                        jwt.sign({email:input.email},"blog-app",{expiresIn:"1d"},
+                            (error,token)=>{
+                                if(error){
+                                    res.json({"status":"unable to create token"})
+                                }
+                                else{
+                                    res.json({"status":"success","userId":response[0]._id,"token":token})
+                                }
+                            })
+                    }
+                    else{
+                        res.json({"status":"incorrect password"})
+                    }
+                })
+            }
+            else{
+                res.json({"status":"user not found"})
+            }
+        }
+    ).catch()
 })
-
 
 
 app.listen(8080, () => {
