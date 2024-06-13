d0f2afda3334162bf55c3d7414b9a9910d303963
swadhi03 <swathiksanthosh03@gmail.com>
2024-06-13 21:22:06 +0530

    completed

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const { usermodel } = require("./models/user")
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://swathi:swathi2609@cluster0.em0miqo.mongodb.net/busdb?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post("/signup", (req, res) => {
+    let input = req.body
+    let user = new usermodel(input)
+    user.save()
+    console.log(user)
+    res.json({ "status": "success" })
+})
+
+app.listen(8080, () => {
+    console.log("server started")
+}
+)
